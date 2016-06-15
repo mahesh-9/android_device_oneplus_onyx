7ea7e47c5d26d8296d970773439b44c440dfc33d
rohitporoli <rohitp4all@gmail.com>
2016-06-16 00:15:46 +0530

    Revert "onyx: init: Log once the props are set"
    This reverts commit c5505aef802318428bb67b60246bb10ce0e2b94d.

--- init/init_onyx.cpp
+++ init/init_onyx.cpp
@@ -36,7 +36,6 @@
 
 void vendor_load_properties()
 {
-    char device[PROP_VALUE_MAX];
     char platform[PROP_VALUE_MAX];
     char rf_version[PROP_VALUE_MAX];
     int rc;
@@ -48,7 +47,7 @@ void vendor_load_properties()
     property_get("ro.boot.rf_version", rf_version);
 
     if (strstr(rf_version, "101")) {
-        /* China */
+        /* Chinese */
         property_set("ro.product.model", "ONE E1001");
         property_set("ro.rf_version", "TDD_FDD_Ch_All");
     } else if (strstr(rf_version, "102")) {
@@ -60,6 +59,4 @@ void vendor_load_properties()
         property_set("ro.product.model", "ONE E1005");
         property_set("ro.rf_version", "TDD_FDD_Am");
     }
-    property_get("ro.product.device", device);
-    INFO("Found rf_version : %s setting build properties for %s device\n", rf_version, device);
 }
